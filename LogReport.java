e5dbf1a2620cc0ee3d2507fdccdd92285fa380ae
GrantSolt <167796584+GrantSolt@users.noreply.github.com>
2024-11-28 23:08:51 -0600

    Add files via upload

---
+++ LogReport.java
@@ -0,0 +1,22 @@
+public class LogReport {
+    private String date;
+    private String log;
+    private String message;
+    public LogReport(String date, String log, String message) {
+        this.date = date;
+        this.log = log;
+        this.message = message;
+    }
+    public String getDate() {
+        return date;
+    }
+    public String getLog() {
+        return log;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public String toString() {
+        return date + " " + log + " " + message;
+    }
+}
